8223981e74b9a1ed47aed6b25e6eb9ae423fc982
Vyacheslav Anishchenko <avuua16@gmail.com>
2023-12-11 20:13:58 +0100

    BREAK=POINT before SQL introduction

--- app/src/main/java/ua/co/a_byte_at_a_bite/montessoritracker/FourthFragment.kt
+++ app/src/main/java/ua/co/a_byte_at_a_bite/montessoritracker/FourthFragment.kt
@@ -5,7 +5,6 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.navigation.fragment.findNavController
 import ua.co.a_byte_at_a_bite.montessoritracker.databinding.FrFourthViewEditActivityBinding
 
 
@@ -25,14 +24,6 @@ class FourthFragment : Fragment() {
         _binding = FrFourthViewEditActivityBinding.inflate(inflater, container, false)
         return binding.root
 
-    }
-
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
-
-        binding.fr4ToFr3Btn.setOnClickListener {
-            findNavController().navigate(R.id.action_fourthFragment_to_thirdFragment)
-        }
     }
 
     override fun onDestroyView() {
